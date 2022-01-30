0b05f5ee42cd319cae04a4c5d80aef04ff4d0ffd
wafiben <wafibenjeddou@gmail.com>
2022-01-30 15:07:37 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import Home from "./Home";
+class App extends Component {
+  state = { count: 0, show: false };
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  decrement = () => {
+    if (this.state.count > 0) {
+      this.setState({ count: this.state.count - 1 });
+    }
+  };
+  componentDidMount = () => {
+    console.log("mounting");
+  };
+  componentDidUpdate = () => {
+    console.log("update");
+  };
+  handleShow = () => {
+    this.state.show
+      ? this.setState({ show: false })
+      : this.setState({ show: true });
+    console.log(this.state.show);
+  };
+  render() {
+    return (
+      <div style={{ textAlign: "center" }}>
+        <button onClick={this.increment}>+</button>
+        {this.state.count}
+        <button onClick={this.decrement}>-</button>
+        <div>
+          <button onClick={this.handleShow}>show</button>
+          {this.state.show ? <Home /> : null}
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+ class Home extends Component {
+     componentWillUnmount=()=>{
+         alert('hello')
+     }
+    render() {
+        return (
+            <div>
+                <h1>this is home page </h1>
+            </div>
+        )
+    }
+}
+export default Home
